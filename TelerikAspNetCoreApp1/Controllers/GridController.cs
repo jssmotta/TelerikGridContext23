48dbb58b68a00804a8863fd33bcb9c0fda3f5ba9
Jefferson S. Motta <55891584+jssmotta@users.noreply.github.com>
2023-11-30 17:26:15 -0300

    Published

---
+++ TelerikAspNetCoreApp1/Controllers/GridController.cs
@@ -0,0 +1,35 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using TelerikAspNetCoreApp1.Models;
+
+namespace TelerikAspNetCoreApp1.Controllers;
+public class GridController : Controller
+{
+    public ActionResult Orders_Read([DataSourceRequest] DataSourceRequest request)
+    {
+        var result = Enumerable.Range(2, 51).Select(i => new OrderViewModel
+        {
+            OrderID = i,
+            Freight = i * 10,
+            OrderDate = new DateTime(2023, 9, 15).AddDays(i % 7),
+            ShipName = "ShipName " + i,
+            ShipCity = "ShipCity " + i
+        });
+
+        var customRet = result.ToList();
+
+        customRet.Insert(0,  new OrderViewModel
+        {
+            OrderID = 1,
+            Freight = 1 * 10,
+            OrderDate = new DateTime(2023, 9, 15).AddDays(1 % 7),
+            ShipName = "Brazil",
+            ShipCity = "Porto Alegre"
+        });
+
+
+        var dsResult = customRet.ToDataSourceResult(request);
+        return Json(dsResult);
+    }
+}
